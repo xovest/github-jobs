6902200a360929c146fbc1a1da4d98a7018086a3
xovest <79008917+xovest@users.noreply.github.com>
2021-11-17 19:50:32 +0200

    job

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import useFetchJobs from "./useFetchJobs";
 import { Container } from 'react-bootstrap';
+import { useState } from "react";
+import Job from "../Job";
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
   const { jobs, loading, error } = useFetchJobs();
 
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   );
 }
