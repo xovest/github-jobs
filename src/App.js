1a209f967aa2e5160642b2d1d154900f584a63b0
xovest <79008917+xovest@users.noreply.github.com>
2021-11-17 19:02:56 +0200

    fetch hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import useFetchJobs from "./useFetchJobs";
+import { Container } from 'react-bootstrap';
+
 function App() {
+  const { jobs, loading, error } = useFetchJobs();
+
   return (
-    <h1>ya setup</h1>
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
   );
 }
 
